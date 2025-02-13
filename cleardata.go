9aedd97fd48c16fa60ebb427809e03a24b9fe3b6
Shion Ichikawa <shion1305@gmail.com>
2025-02-13 18:21:13 +1100

    ✨ cleardata.go: Firestore Emulator util for clearing data

---
+++ cleardata.go
@@ -0,0 +1,44 @@
+package emutils
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type EmulatorInfo struct {
+	Host      string
+	Port      int
+	ProjectID string
+}
+
+func NewEmulator(
+	Host string,
+	Port int,
+	ProjectID string,
+) EmulatorInfo {
+	return EmulatorInfo{
+		Host, Port, ProjectID,
+	}
+}
+
+// ClearData sends DELETE request to emulator host
+func (info EmulatorInfo) ClearData() error {
+	url := fmt.Sprintf("http://%s:%d/emulator/v1/projects/%s/databases/(default)/documents",
+		info.Host, info.Port, info.ProjectID)
+	req, err := http.NewRequest(
+		"DELETE",
+		url,
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("faield to create request: %v", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("faield to send request: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+	return nil
+}
