a29590250fe09e6884bc2036d1ff78e2dd8b90ea
Shion Ichikawa <shion1305@gmail.com>
2025-04-27 15:17:01 +0900

    ♻️ refactor ResetEmulator, add validation for projectID

--- reset_emulator.go
+++ reset_emulator.go
@@ -7,18 +7,45 @@ import (
 	"regexp"
 )
 
-const regexHost = `[^:]+:\d{1,5}`
+var regexHost, regexProjID regexp.Regexp
+
+func init() {
+	regexHost = *regexp.MustCompile(`^[^:]+:\d{1,5}$`)
+	regexProjID = *regexp.MustCompile(`^[a-z][a-z0-9-]{4,28}[a-z0-9]$`)
+}
 
 func ResetEmulator(projectID string) error {
-	emuHost := os.Getenv("FIRESTORE_EMULATOR_HOST")
-	if emuHost == "" {
-		return fmt.Errorf("missing FIRESTORE_EMULATOR_HOST")
+	if !regexProjID.MatchString(projectID) {
+		return fmt.Errorf("project ID must match %s, %s", regexProjID.String(), projectID)
 	}
-	re := regexp.MustCompile(regexHost)
-	if !re.MatchString(emuHost) {
-		return fmt.Errorf("invalid FIRESTORE_EMULATOR_HOST, should be host:port but got: %s", emuHost)
+	emuHost, err := loadEmulatorHostEnv()
+	if err != nil {
+		return err
 	}
+	return execResetReq(emuHost, projectID)
+}
 
+func loadEmulatorHostEnv() (host string, err error) {
+	emuHost := os.Getenv("FIRESTORE_EMULATOR_HOST")
+	emuPort := os.Getenv("FIRESTORE_EMULATOR_PORT")
+	if emuHost == "" {
+		if emuPort == "" {
+			// if all variables are not set, throw error
+			return "", fmt.Errorf("missing FIRESTORE_EMULATOR_HOST")
+		}
+		return fmt.Sprintf("localhost:%s", emuPort), nil
+	}
+	// validate if Host is in the format of host:port
+	if regexHost.MatchString(emuHost) {
+		return emuHost, nil
+	}
+	if emuPort == "" {
+		return "", fmt.Errorf("invalid FIRESTORE_EMULATOR_HOST, should be host:port but got: %s", emuHost)
+	}
+	return fmt.Sprintf("%s:%s", emuHost, emuPort), nil
+}
+
+func execResetReq(emuHost, projectID string) error {
 	url := fmt.Sprintf(
 		"http://%s/emulator/v1/projects/%s/databases/(default)/documents",
 		emuHost, projectID,
@@ -31,6 +58,7 @@ func ResetEmulator(projectID string) error {
 	if err != nil {
 		return fmt.Errorf("failed to send request: %v", err)
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
